a0aed72f64baf131cd63eda1357fbc4ae4ab6c6e
RFerlin <10963428@uvu.edu>
2023-11-14 22:12:27 -0700

    Added Dune. Animated Dune. Started Anim State Machine

--- FallProject/Assets/Scripts/PlayerController.cs
+++ FallProject/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+    public float jumpForce;
+   
+    public float gravityScale = 5f;
+    
+    public CharacterController charController;
+    
+    private Vector3 moveDirection;
+
+    private Camera theCam;
+    
+    public GameObject playerModel;
+    public float rotateSpeed;
+    
+    public Animator anim;
+    
+    private void Start()
     {
-        
+        theCam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        float yStore = moveDirection.y;
+        //moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveDirection.Normalize();
+        moveDirection = moveDirection * moveSpeed;
+        moveDirection.y = yStore;
         
+        if (Input.GetButtonDown("Jump"))
+        { 
+            moveDirection.y = jumpForce;
+        }
+        
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+        
+        //transform.position += moveDirection * (moveSpeed * Time.deltaTime);
+
+        charController.Move(moveDirection * Time.deltaTime);
+        
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, theCam.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
     }
 }
